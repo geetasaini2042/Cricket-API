53b63eca1225db070a0c99e03e3f306906fd4c90
geetasaini2042 <Geetasaini2042@gmail.com>
2025-09-29 22:52:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -166,5 +166,8 @@ def live_matches():
 def website():
     return render_template('index.html')
 
-if __name__ =="__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
